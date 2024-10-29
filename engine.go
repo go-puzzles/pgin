b33b5c470bbb8b2b655a60331572dd881f937f18
Hoven <hovenyang.best@gmail.com>
2024-10-29 14:35:33 +0800

    feat: add an optional way to create HTTP services

--- engine.go
+++ engine.go
@@ -8,7 +8,12 @@
 
 package pgin
 
-import "github.com/gin-gonic/gin"
+import (
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Default(opts ...gin.OptionFunc) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
@@ -16,4 +21,78 @@ func Default(opts ...gin.OptionFunc) *gin.Engine {
 	engine := gin.New()
 	engine.Use(LoggerMiddleware(), gin.Recovery())
 	return engine.With(opts...)
+}
+
+func NewServerHandler() *gin.Engine {
+	engine := gin.New()
+
+	// default health check
+	engine.GET("/health", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	return engine
+}
+
+func NewStandardServerHandler() *gin.Engine {
+	engine := NewServerHandler()
+	engine.Use(LoggerMiddleware(), gin.Recovery())
+	return engine
+}
+
+func NewServerHandlerWithOptions(opts ...Option) *gin.Engine {
+	engine := NewServerHandler()
+	for _, opt := range opts {
+		opt(engine)
+	}
+	return engine
+}
+
+type Option func(*gin.Engine)
+
+type Router interface {
+	Init(gin.IRouter)
+}
+
+func WithRouters(basePath string, routers ...Router) Option {
+	return func(e *gin.Engine) {
+		group := e.Group(basePath)
+		for _, router := range routers {
+			router.Init(group)
+		}
+	}
+}
+
+func WithMiddlewares(middlewares ...gin.HandlerFunc) Option {
+	return func(e *gin.Engine) {
+		for _, m := range middlewares {
+			e.Use(m)
+		}
+	}
+}
+
+func WithLoggingRequest(header bool) Option {
+	return func(e *gin.Engine) {
+		e.Use(LoggingRequest(header))
+	}
+}
+
+func WithReuseBody() Option {
+	return func(e *gin.Engine) {
+		e.Use(ReuseBody())
+	}
+}
+
+func WithServiceName(name string) Option {
+	return func(engine *gin.Engine) {
+		engine.Use(func(c *gin.Context) {
+			c.Set("service", name)
+		})
+	}
+}
+
+func WithHiddenRoutesLog() Option {
+	return func(engine *gin.Engine) {
+		gin.DefaultWriter = io.Discard
+	}
 }
